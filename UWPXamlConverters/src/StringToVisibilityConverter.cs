006aea8b507bd19e7cad4540a940bd14461ce2f6
vignesh.govindhan <vignesh.govindhan@zohocorp.com>
2019-06-29 16:55:30 +0530

    Added StringToVisibilityConverter along with sample

--- UWPXamlConverters/src/StringToVisibilityConverter.cs
+++ UWPXamlConverters/src/StringToVisibilityConverter.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
 
 namespace UWPXamlConverters.Converters
 {
-    class StringToVisibilityConverter
+    public class StringtovisbilityConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType,
+            object parameter, string language)
+        {
+            return (!string.IsNullOrEmpty((string)value)) && value.ToString().Equals(parameter.ToString()) ? Visibility.Visible : Visibility.Collapsed;
+        }
+        // ConvertBack is not implemented for a OneWay binding.
+        public object ConvertBack(object value, Type targetType,
+            object parameter, string language)
+        {
+            return value;
+        }
     }
 }
