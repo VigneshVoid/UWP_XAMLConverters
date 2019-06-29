3ab1f261fe6f4dfa7def5b51bb4ec014fd464e87
vignesh.govindhan <vignesh.govindhan@zohocorp.com>
2019-06-29 17:07:21 +0530

    Added StringToVisibiltyConverter Reverse and its sample

---
+++ UWPXamlConverters/src/StringToVisibilityReverseConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace UWPXamlConverters.Converters
+{
+    public class StringToVisibilityReverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, string language)
+        {
+            return value == null || !(value.ToString().Equals(parameter.ToString())) ? Visibility.Visible : Visibility.Collapsed;
+        }
+        // ConvertBack is not implemented for a OneWay binding.
+        public object ConvertBack(object value, Type targetType,
+            object parameter, string language)
+        {
+            return value;
+        }
+    }
+}
