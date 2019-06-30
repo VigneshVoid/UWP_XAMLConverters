6e62f8a62438532f312333622577edc4d1c6f7c8
vignesh.govindhan <vignesh.govindhan@zohocorp.com>
2019-06-30 14:49:18 +0530

    StringToBoolReverseConverter

---
+++ UWPXamlConverters/src/StringToBoolReverseConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace UWPXamlConverters.Converters
+{
+    public class StringToBoolReverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, string language)
+        {
+            return value == null || !(value.ToString().Equals(parameter.ToString()));
+        }
+        public object ConvertBack(object value, Type targetType,
+            object parameter, string language)
+        {
+            return value;
+        }
+    }
+}
